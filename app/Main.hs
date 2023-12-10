e608456fa7bc42a990fd0ae754b9c7cf16ab4fe3
wolewnicki <wolewnicki@yahoo.com>
2023-12-09 20:09:19 -0500

    restructured project to make it easier to print solutions

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,27 @@
 module Main (main) where
 
-import Lib (displaySolutions)
+import qualified Solutions.Year2023.Day1 as Day1
+-- import qualified Solutions.Year2023.Day2 as Day2
 
 main :: IO ()
-main = displaySolutions
+main = do
+  putStrLn "Solutions:"
+  putStrLn ""
+
+  doDay 2023 1 Day1.problem1 Day1.problem2
+  -- doDay 2023 2 Day2.problem1 Day2.problem2
+
+  putStrLn "End Solutions"
+
+doDay :: Show a => Int -> Int -> (String -> a) -> (String -> a) -> IO ()
+doDay year day problem1 problem2 = do
+  let title = unwords ["Year", show year, "Day", show day]
+  input <- readFile $ mconcat ["inputs/year", show year, "/day", show day, "/input.txt"]
+  printSolution (title ++ " Problem 1") $ problem1 input
+  printSolution (title ++ " Problem 2") $ problem2 input
+
+printSolution :: Show a => String -> a -> IO ()
+printSolution title solution = do
+  putStrLn $ "Problem: " ++ title
+  putStrLn $ "solution: " ++ show solution
+  putStrLn ""
---
+++ src/Solutions/Year2023/Day1.hs
@@ -0,0 +1,62 @@
+module Solutions.Year2023.Day1
+  ( problem1
+  , problem2
+  ) where
+
+import Data.Char (isNumber)
+import Data.List (elemIndex)
+import Control.Monad (guard)
+
+stringToInt :: String -> Int
+stringToInt s = read s :: Int
+
+getFirstLast :: String -> String
+getFirstLast s = [head s, last s]
+
+firstLastToInt :: String -> Int
+firstLastToInt = stringToInt . getFirstLast . filter isNumber
+
+firstLastToIntPart2 :: String -> Int
+firstLastToIntPart2 = stringToInt . getFirstLast . getNumberOrNumberWords
+
+getNumberOrNumberWords :: String -> String
+getNumberOrNumberWords s = concatMap numberWordToDigit $ extractNumbersAndNumberWords s
+
+day1Solve :: (String -> Int) -> String -> Int
+day1Solve firstLastFn input = sum $ map firstLastFn $ words input
+
+problem1 :: String -> Int
+problem1 = day1Solve firstLastToInt
+
+problem2 :: String -> Int
+problem2 = day1Solve firstLastToIntPart2
+
+numberWords :: [String]
+numberWords =
+  [ "one"
+  , "two"
+  , "three"
+  , "four"
+  , "five"
+  , "six"
+  , "seven"
+  , "eight"
+  , "nine"
+  ]
+
+numbers :: [String]
+numbers = map show [1..9]
+
+
+numberWordToDigit :: String -> String
+numberWordToDigit word = maybe word (show . (+ 1)) (elemIndex word numberWords)
+
+extractNumbersAndNumberWords :: String -> [String]
+extractNumbersAndNumberWords s = do
+  start <- [0..(length s - 1)]
+  len <- [1..(min 5 (length s - start))]
+
+  let word = take len (drop start s)
+
+  guard $ word `elem` numberWords || word `elem` numbers
+  return word
